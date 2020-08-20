7f23c8a0c885e9494511340381431b55342d9a8c
Pqvqn <pavanarauch@hotmail.com>
2020-08-19 22:08:50 -0400

    more InputDraw improvements
    button highlight is outline, colors can now correctly be set to pure
    black/white, and 10 slots to save/use colors (right click to save pen
    color, left click to set)

--- RockStickLeaf/src/ui/UIButton.java
+++ RockStickLeaf/src/ui/UIButton.java
@@ -9,16 +9,18 @@ import rsl.*;
 public class UIButton extends UIElement{
 
 	private UIRectangle backing;
+	private UIRectangle overlay;
 	private UIText text;
 	private boolean highlight;
 	//private boolean pressed;
 	private Color back, hlight;
 	
-	public UIButton(Game frame, int x, int y, int w, int h, Color bc, Color bch, String t, Color tc, Font f) {
+	public UIButton(Game frame, int x, int y, int w, int h, Color backc, Color overc, boolean filloverlay, String t, Color tc, Font f) {
 		super(frame,x,y);
-		back = bc;
-		hlight = bch;
-		parts.add(backing = new UIRectangle(game,xPos-w/2,yPos-h/2,w,h,bc,true));
+		back = backc;
+		hlight = overc;
+		parts.add(backing = new UIRectangle(game,xPos-w/2,yPos-h/2,w,h,back,true));
+		parts.add(overlay = new UIRectangle(game,xPos-w/2,yPos-h/2,w,h,hlight,filloverlay));
 		parts.add(text = new UIText(game,xPos,yPos+f.getSize()/3,t,tc,f));
 		update();
 	}
@@ -26,6 +28,11 @@ public class UIButton extends UIElement{
 	
 	public boolean highlighted() {return highlight;}
 	public String text() {return text.getText();}
+	public Color buttonColor() {return back;}
+	public void setColor(Color c) {
+		back = c;
+		backing.setColor(back);
+	}
 	
 	@Override
 	public void update() {
@@ -36,9 +43,9 @@ public class UIButton extends UIElement{
 		double y = b.getY();
 		highlight = x>=backing.getxPos() && x<=backing.getxPos()+backing.getwLen() && y>=backing.getyPos() && y<=backing.getyPos()+backing.gethLen();
 		if(highlight) { //color based on if mouse is hovered
-			backing.setColor(hlight);
+			overlay.setColor(hlight); //show highlight
 		}else {
-			backing.setColor(back);
+			overlay.setColor(new Color(0,0,0,0)); //remove highlight
 		}
 	}
 }
--- RockStickLeaf/src/ui/UIInputDraw.java
+++ RockStickLeaf/src/ui/UIInputDraw.java
@@ -36,15 +36,18 @@ public class UIInputDraw extends UIElement implements MouseListener{
 		
 		//create all buttons
 		buttons = new ArrayList<UIButton>();
-		buttons.add(new UIButton(game,xPos,yPos-230,100,60,Color.BLACK,Color.YELLOW,"Submit",Color.WHITE,new Font("Arial",Font.BOLD,20))); //submit
-		buttons.add(new UIButton(game,xPos-wid/5,yPos+170,40,40,Color.BLACK,Color.YELLOW,"-",Color.WHITE,new Font("Arial",Font.BOLD,20))); //pen size down
-		buttons.add(new UIButton(game,xPos+wid/5,yPos+170,40,40,Color.BLACK,Color.YELLOW,"+",Color.WHITE,new Font("Arial",Font.BOLD,20))); //pen size up
-		buttons.add(new UIButton(game,xPos-wid/5,yPos+220,40,40,Color.BLACK,Color.YELLOW,"R+",Color.WHITE,new Font("Arial",Font.BOLD,20))); //pen color red up
-		buttons.add(new UIButton(game,xPos,yPos+220,40,40,Color.BLACK,Color.YELLOW,"G+",Color.WHITE,new Font("Arial",Font.BOLD,20))); //pen color green up
-		buttons.add(new UIButton(game,xPos+wid/5,yPos+220,40,40,Color.BLACK,Color.YELLOW,"B+",Color.WHITE,new Font("Arial",Font.BOLD,20))); //pen color blue up
-		buttons.add(new UIButton(game,xPos-wid/5,yPos+300,40,40,Color.BLACK,Color.YELLOW,"R-",Color.WHITE,new Font("Arial",Font.BOLD,20))); //pen color red down
-		buttons.add(new UIButton(game,xPos,yPos+300,40,40,Color.BLACK,Color.YELLOW,"G-",Color.WHITE,new Font("Arial",Font.BOLD,20))); //pen color green down
-		buttons.add(new UIButton(game,xPos+wid/5,yPos+300,40,40,Color.BLACK,Color.YELLOW,"B-",Color.WHITE,new Font("Arial",Font.BOLD,20))); //pen color blue down
+		buttons.add(new UIButton(game,xPos,yPos-230,100,60,Color.BLACK,Color.YELLOW,false,"Submit",Color.WHITE,new Font("Arial",Font.BOLD,20))); //submit
+		buttons.add(new UIButton(game,xPos-wid/5,yPos+170,40,40,Color.BLACK,Color.YELLOW,false,"-",Color.WHITE,new Font("Arial",Font.BOLD,20))); //pen size down
+		buttons.add(new UIButton(game,xPos+wid/5,yPos+170,40,40,Color.BLACK,Color.YELLOW,false,"+",Color.WHITE,new Font("Arial",Font.BOLD,20))); //pen size up
+		buttons.add(new UIButton(game,xPos-wid/5,yPos+220,40,40,Color.BLACK,Color.YELLOW,false,"R+",Color.WHITE,new Font("Arial",Font.BOLD,20))); //pen color red up
+		buttons.add(new UIButton(game,xPos,yPos+220,40,40,Color.BLACK,Color.YELLOW,false,"G+",Color.WHITE,new Font("Arial",Font.BOLD,20))); //pen color green up
+		buttons.add(new UIButton(game,xPos+wid/5,yPos+220,40,40,Color.BLACK,Color.YELLOW,false,"B+",Color.WHITE,new Font("Arial",Font.BOLD,20))); //pen color blue up
+		buttons.add(new UIButton(game,xPos-wid/5,yPos+300,40,40,Color.BLACK,Color.YELLOW,false,"R-",Color.WHITE,new Font("Arial",Font.BOLD,20))); //pen color red down
+		buttons.add(new UIButton(game,xPos,yPos+300,40,40,Color.BLACK,Color.YELLOW,false,"G-",Color.WHITE,new Font("Arial",Font.BOLD,20))); //pen color green down
+		buttons.add(new UIButton(game,xPos+wid/5,yPos+300,40,40,Color.BLACK,Color.YELLOW,false,"B-",Color.WHITE,new Font("Arial",Font.BOLD,20))); //pen color blue down
+		for(int i=0; i<10; i++) {
+			buttons.add(new UIButton(game,xPos-70+(i%5*35),yPos+360+(i/5*35),30,30,Color.BLACK,Color.YELLOW,false,"#",Color.WHITE,new Font("Arial",Font.BOLD,20))); //pen color blue down
+		}
 		
 		for(UIButton b : buttons)parts.add(b);
 		
@@ -106,22 +109,29 @@ public class UIInputDraw extends UIElement implements MouseListener{
 						penWid.setText((stroke+1)+"");
 					break;
 				case "R+": //pen color red up
-					penCol.setColor(new Color((color.getRed()+16)%256,color.getGreen()%256,color.getBlue()%256));
+					penCol.setColor(new Color(color.getRed()+15>255?color.getRed()-255:color.getRed()+15,color.getGreen(),color.getBlue()));
 					break;
 				case "R-": //pen color red down
-					penCol.setColor(new Color((color.getRed()+256-16)%256,color.getGreen()%256,color.getBlue()%256));
+					penCol.setColor(new Color(color.getRed()-15<0?color.getRed()+255:color.getRed()-15,color.getGreen(),color.getBlue()));
 					break;
 				case "G+": //pen color green up
-					penCol.setColor(new Color(color.getRed()%256,(color.getGreen()+16)%256,color.getBlue()%256));
+					penCol.setColor(new Color(color.getRed(),color.getGreen()+15>255?color.getGreen()-255:color.getGreen()+15,color.getBlue()));
 					break;
 				case "G-": //pen color green down
-					penCol.setColor(new Color(color.getRed()%256,(color.getGreen()+256-16)%256,color.getBlue()%256));
+					penCol.setColor(new Color(color.getRed(),color.getGreen()-15<0?color.getGreen()+255:color.getGreen()-15,color.getBlue()));
 					break;
 				case "B+": //pen color blue up
-					penCol.setColor(new Color(color.getRed()%256,color.getGreen()%256,(color.getBlue()+16)%256));
+					penCol.setColor(new Color(color.getRed(),color.getGreen(),color.getBlue()+15>255?color.getBlue()-255:color.getBlue()+15));
 					break;
 				case "B-": //pen color blue down
-					penCol.setColor(new Color(color.getRed()%256,color.getGreen()%256,(color.getBlue()+256-16)%256));
+					penCol.setColor(new Color(color.getRed(),color.getGreen(),color.getBlue()-15<0?color.getBlue()+255:color.getBlue()-15));
+					break;
+				case "#": //pen color preset
+					if(arg0.getButton() == MouseEvent.BUTTON3) { //set color storage to current pen color with right click
+						buttons.get(i).setColor(color);
+					}else { //set pen color to color storage otherwise
+						penCol.setColor(buttons.get(i).buttonColor());
+					}
 					break;
 				}
 			}
